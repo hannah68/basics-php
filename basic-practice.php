5bd30f1137e0f52122c645506aa457ab315746fb
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-17 13:00:18 +0100

    init basics of php concept

---
+++ basic-practice.php
@@ -0,0 +1,64 @@
+<?php
+// echo strlen("Hello world!");
+// echo str_word_count("Hello world!");
+// echo strrev("Hello world!");
+// echo strpos("Hello world!", "world");
+// echo str_replace("world", "Dolly", "Hello world!");
+// // $x = 1.9e411;
+// $x = acos(8);
+// var_dump(is_int($x));
+// var_dump(is_float($x));
+// var_dump(is_nan($x));
+
+// $x = 5985;
+// var_dump(is_numeric($x));
+// echo "<br>";
+
+// $x = "5985";
+// var_dump(is_numeric($x));
+// echo "<br>";
+
+// $x = "59.85" + 100;
+// var_dump(is_numeric($x));
+// echo "<br>";
+
+// $x = "Hello";
+// var_dump(is_numeric($x));
+// echo "<br>";
+
+// // Cast float to int
+// $x = 23465.768;
+// $int_cast = (int)$x;
+// echo $int_cast;
+// echo "<br>";
+
+// // Cast string to int
+// $x = "23465.768";
+// $int_cast = (int)$x;
+// echo $int_cast;
+// echo "<br>";
+
+// // round to the nearest num
+// echo(round(0.60));  // returns 1
+// echo "<br>";
+
+// random num between 10,100
+// echo(rand(10, 100));
+
+// create a constant with a case-sensitive name
+// define("GREETING", "Welcome to W3Schools.com!");
+// echo GREETING;
+
+// Constant Arrays
+// define("cars", ["Alfa Romeo","BMW","Toyota"]);
+// echo cars[0];
+
+// Constants are automatically global and can be used across the entire script.
+// define("GREETING", "Welcome to W3Schools.com!");
+// function myTest() {
+//   echo GREETING;
+// }
+// myTest();
+
+
+?>
